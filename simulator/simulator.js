c75e47640e161d7e40ddbf10a5ac58c13933ded0
Isaac Owens <isaac.owens832@gmail.com>
2020-10-24 22:50:22 -0400

    Add generateElevators test suite

--- simulator/simulator.js
+++ simulator/simulator.js
@@ -1,9 +1,22 @@
 const {
   generateTimeSeries
 } = require('./simulator_utils');
+const Elevator = require('../elevator/elevator');
+
 
 class Simulator {
 
+  generateElevators() {
+    const elevatorOne = new Elevator();
+    const elevatorTwo = new Elevator();
+    const elevatorThree = new Elevator();
+    const elevators = [elevatorOne, elevatorTwo, elevatorThree];
+    return elevators;
+  }
+
+  run() {
+
+  }
 }
 
 module.exports = Simulator;
--- test/simulator_test.js
+++ test/simulator_test.js
@@ -1,11 +1,32 @@
 const assert = require('assert');
 const expect = require('chai').expect;
+const Elevator = require('../elevator/elevator');
 const Simulator = require('../simulator/simulator');
 
 const newSimulator = new Simulator();
 
 describe('Simulator', function() {
-  it('create a new Simulator', function() {
+  it('can create a new Simulator', function() {
     assert.ok(newSimulator);
+  });
+
+  describe('#generateElevators', function() {
+    it('should exist as a function', function() {
+      expect(newSimulator.generateElevators()).to.exist;
+    });
+
+    it('should return an array', function() {
+      const elevators = newSimulator.generateElevators();
+      expect(typeof elevators === 'object').to.equal(true);
+    });
+
+    it('should return an array of three Elevators', function() {
+      const elevators = newSimulator.generateElevators();
+      expect(elevators).to.have.lengthOf(3);
+      expect(elevators[0]).to.be.an.instanceOf(Elevator);
+      expect(elevators[1]).to.be.an.instanceOf(Elevator);
+      expect(elevators[2]).to.be.an.instanceOf(Elevator);
+    })
   })
+  
 })
