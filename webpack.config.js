057e87c3f783f87a200857a3f949a58398ce67c6
Isaac Owens <isaac.owens832@gmail.com>
2020-10-25 12:54:42 -0400

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  entry: ["babel-polyfill", "./src/scripts/simulator/simulator.js"],
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(wav)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "sounds/",
+            publicPath: "./dist/sounds/",
+          },
+        },
+      },
+      {
+        test: /\.(png)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "images/",
+            publicPath: "./dist/images/",
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", "*"],
+  },
+  devServer: {
+    contentBase: ".",
+  },
+};
